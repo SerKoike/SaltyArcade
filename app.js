841cd50d47fb81eb3501a8e482c059e22d546a0d
SerKoike <kyongedd@gmail.com>
2017-01-05 17:34:52 +0100

    FINAL to send, Should be in verification branch

--- app.js
+++ app.js
@@ -4,17 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var digger = require('./routes/digger');
-var oregon = require('./routes/oregontrails');
-var space = require('./routes/spaceinvader');
-var wf3d = require('./routes/wolfenstein');
-
 var mongoose = require('mongoose');
 var session = require('express-session');
-
+var arkanoid = require('./routes/arkanoid');
 var app = express();
 
 app.use(session({ 
@@ -37,9 +31,9 @@ app.use(function(req,res,next){
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine("html",require("ejs").__express);
-app.set('view engine', 'html');
-//app.set('view engine', 'ejs');
+//app.engine("html",require("ejs").__express);
+//app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -50,43 +44,47 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//Link to pages :
-//in folder 'views' is the html
-//in folder 'route' is the javascript
-app.use('/digger', digger);
-app.use('/oregontrails', oregon);
-app.use('/spaceinvader', space);
-app.use('/wolfenstein', wf3d);
-
 app.use('/', routes);  
 app.use('/users', users); 
 app.use('/login',routes); 
 app.use('/register',routes); 
 app.use('/home',routes); 
-app.use("/logout",routes); 
+app.use('/logout', routes);
+app.use('/arkanoid', arkanoid);
+app.use('/digger', routes);
+app.use('/oregontrails', routes);
+app.use('/spaceinvader', routes);
+app.use('/wolfenstein', routes);
+
+
+
+
 
 global.dbHandel = require('./database/dbHandel');
 global.db = mongoose.connect("mongodb://localhost/nodedb");
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
