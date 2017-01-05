3b79bc86cd2f72d5dc163a349da2340cf14813bc
jdwsj <jd_wsj@126.com>
2017-01-05 11:12:55 +0800

    Intergrate Login
    Intergrate Login

--- app.js
+++ app.js
@@ -4,26 +4,65 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var mongoose = require('mongoose');
+var session = require('express-session');
 var app = express();
+
+app.use(session({ 
+    secret: 'secret',
+    cookie:{ 
+        maxAge: 1000*60*30
+    }
+}));
+
+app.use(function(req,res,next){ 
+    res.locals.user = req.session.user;   
+    var err = req.session.error;   
+    delete req.session.error;
+    res.locals.message = "";
+    if(err){ 
+        res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
+    }
+    next();  
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine("html",require("ejs").__express);
+app.set('view engine', 'html');
+//app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
+app.use('/', routes);  
+app.use('/users', users); 
+app.use('/login',routes); 
+app.use('/register',routes); 
+app.use('/home',routes); 
+app.use("/logout",routes); 
+
+
+
+
+
+
+global.dbHandel = require('./database/dbHandel');
+global.db = mongoose.connect("mongodb://localhost/nodedb");
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
