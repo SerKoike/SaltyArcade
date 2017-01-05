14df7e655954f9ab382ac04bcb7c792680579412
SerKoike <kyongedd@gmail.com>
2017-01-05 09:51:49 +0100

    commit test using git shell due to branching desync

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var digger = require('./routes/digger');
+var oregon = require('./routes/oregontrails');
+var space = require('./routes/spaceinvader');
+var wf3d = require('./routes/wolfenstein');
+
+
 var app = express();
 
 // view engine setup
@@ -22,25 +28,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Link to pages :
+//in folder 'views' is the html
+//in folder 'route' is the javascript
 app.use('/', index);
 app.use('/users', users);
+app.use('/digger', digger);
+app.use('/oregontrails', oregon);
+app.use('/spaceinvader', space);
+app.use('/wolfenstein', wf3d);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
